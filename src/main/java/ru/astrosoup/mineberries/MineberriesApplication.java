5ba50ec3e1116ed413cbb1afd6c6c31eb68f6ed8
Semyon Gorin <1234snsks@gmail.com>
2026-09-22 17:12:55 +0300

    init

---
+++ src/main/java/ru/astrosoup/mineberries/MineberriesApplication.java
@@ -0,0 +1,13 @@
+package ru.astrosoup.mineberries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineberriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MineberriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/astrosoup/mineberries/MineberriesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.astrosoup.mineberries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineberriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
